a45a66804d4fc23d10dd296f24a3b3aa95dd3a6f
Michael-Is-Hungry <68034273+Michael-Is-Hungry@users.noreply.github.com>
2021-11-19 09:11:36 -0800

    Updated code for more speed and readability.

--- 3x3 Graphs/Graph 11/3 Loops/A/main.py
+++ 3x3 Graphs/Graph 11/3 Loops/A/main.py
@@ -1,9 +1,10 @@
 import sympy as sp
 import random as rd
+import fractions as fr
 
 #################################################################################
 # Author: Michael Hefley
-# Date: 11/10/2021
+# Date: 11/18/2021
 # Graph: 11 w/ 3 loops
 # Nilpotent Signing: A
 #
@@ -11,127 +12,96 @@ import random as rd
 # Arbitrary Pattern (SCSAP) of a specific graph (or signed graph). The program 
 # generates random coefficients for our characteristic polynomial and then runs
 # through every possible signing of the 3 coefficients and saves a desired 
-# realization to the "outout.txt" file. Sections that may need to be changed 
+# realization to the "output.txt" file. Sections that may need to be changed
 # when switching to a different graph contain double asterisks around the comments 
 # above that section. Remember, this program does NOT show if a graph is NOT
 # SCSAP, but can show that it is if ALL 27 realizations are found.
 #################################################################################
 
+
 def main():
-  a, b, c, d, e, f, g, u, v, w = sp.symbols("a b c d e f g u v w")
+    realization_chars = ['-', '0', '+']
 
-  # If you wish to clean up the file for a new run, uncomment the cleanFile() function below.
-  cleanFile()
+    # If you wish to clean up the file for a new run, uncomment the cleanFile() function below.
+    clean_file()
 
-  # **Set the "free" variables with correct signing**
-  c = 1
-  d = 2
+    # **Set the "free" variables with correct signing**
+    c = fr.Fraction(1)
+    d = fr.Fraction(2)
 
-  trueVals = 0
-  while True:
-    # Chooses u,v,w to ba a random integer.
-    uTemp = rd.randint(1,10)
-    vTemp = rd.randint(1,10)
-    wTemp = rd.randint(1,10)
+    while True:
+        # Chooses u,v,w to ba a random integer.
+        u_temp = fr.Fraction(rd.randint(1, 10))
+        v_temp = fr.Fraction(rd.randint(1, 10))
+        w_temp = fr.Fraction(rd.randint(1, 10))
 
-    # **Ensure we eliminate all possibilities of dividing by 0 and obtaining imaginary numbers.**
-    if (uTemp != (-c-2*d) and -1*uTemp != (-c-2*d)):
-      for x in range(3): # (0,+,-) for the u value
+        # **Ensure we eliminate all possibilities of dividing by 0 and obtaining imaginary numbers.**
+        if u_temp != -c - 2 * d and -1 * u_temp != -c - 2 * d:
+            for x, index1 in enumerate(realization_chars):  # (-,0,+) for the u value
+                u = (x - 1) * u_temp
 
-        u = uTemp
-        if (x==0): # Makes u = 0
-          u = 0
-          uStr = "0"
-        elif (x==1): # Makes u positive
-          u = abs(u)
-          uStr = "+"
-        else: # Makes u negative
-          u = -1*abs(u)
-          uStr = "-"
+                for y, index2 in enumerate(realization_chars):  # (-,0,+) for the v value
+                    v = (y - 1) * v_temp
 
-        for y in range(3): # (0,+,-) for the v value
+                    for z, index3 in enumerate(realization_chars):  # (-,0,+) for the w value
+                        w = (z - 1) * w_temp
 
-          v = vTemp
-          if (y==0): # Makes v = 0
-            v = 0
-            vStr = "0"
-          elif (y==1): # Makes v positive
-            v = abs(v)
-            vStr = "+"
-          else: # Makes v negative
-            v = -1*abs(v)
-            vStr = "-"
+                        # **Set variables to make coefficients of charpoly be u,v, and w.**
+                        e = -c - d - u
+                        b = (-2 * c ** 2 * u - 4 * c * d * u - c * u ** 2 - c * v - 2 * d ** 2 * u - d * u ** 2 - d * v
+                             - u * v + w - (c + d) ** 3) / (c + 2 * d + u)
+                        a = -(d ** 3 + d ** 2 * u + d * v + w) / (c + 2 * d + u)
 
-          for z in range(3): # (0,+,-) for the w value
-           
-            w = wTemp
-            if (z==0): # Makes w = 0
-              w = 0
-              wStr = "0"
-            elif (z==1): # Makes w positive
-              w = abs(w)
-              wStr = "+"
-            else: # Makes w negative
-              w = -1*abs(w)
-              wStr = "-"
+                        # **Creates matrix and sign pattern.**
+                        matrix = sp.Matrix([[c, 1, b], [a, d, 0], [1, 0, e]])
 
-            # **Set variables to make coefficients of charpoly be u,v, and w.**
-            e = -c-d-u
-            b = (-2*c**2*u - 4*c*d*u - c*u**2 - c*v - 2*d**2*u - d*u**2 - d*v - u*v + w - (c + d)**3)/(c + 2*d + u)
-            a = -(d**3 + d**2*u + d*v + w)/(c + 2*d + u)
+                        # Makes a copy of the file.
+                        with open("output.txt", 'r') as file:
+                            data = file.readlines()
 
-            # **Creates matrix and sign pattern.**
-            A = sp.Matrix([[c,1,b],[a,d,0],[1,0,e]])
-            realizationPattern = "{" + uStr + "," + vStr + "," + wStr + "}"
+                        # **Checks to see if our variables have the correct signing and then checks to see
+                        # if the spot in the text file is taken for that realization pattern. If its not
+                        # then we edit our copy of the file with our found realization then overwrite the
+                        # actually file.**
+                        if a < 0 and b < 0 and e < 0:
+                            if data[5 * (9 * x + 3 * y + z)] == '\n':
 
-            # Makes a copy of the file.
-            with open("output.txt", 'r') as file:
-              data = file.readlines()
+                                # **Section that writes to the copy of the file.
+                                data[(5 * (9 * x + 3 * y + z))] = f'Realization {{{realization_chars[x]},' \
+                                                                  f'{realization_chars[y]},' \
+                                                                  f'{realization_chars[z]}}} \n'
 
-            # **Checks to see if our variables have the correct signing and then checks to see
-            # if the spot in the text file is taken for that realization pattern. If its not 
-            # then we edit our copy of the file with our found realization then overwrite the 
-            # actually file.**
-            if (a < 0 and b < 0 and e < 0):
-              if (data[5*(9*x + 3*y + z)] == '\n'):
-                trueVals = trueVals + 1
+                                data[(1 + 5 * (9 * x + 3 * y + z))] = f'{u = }, {v = }, {w = } \n'
 
-                # **Section that writes to the copy of the file. 
-                data[(5*(9*x + 3*y + z))] = "Realization " + realizationPattern + '\n'
+                                data[(2 + 5 * (9 * x + 3 * y + z))] = f'{a = }, {b = }, {c = }, {d = }, {e = } \n'
 
-                data[(1 + 5*(9*x + 3*y + z))] = "u = " + str(u) + ", v = " + str(v) + ", w = " + str(w) + '\n'
+                                data[(3 + 5 * (9 * x + 3 * y + z))] = f'{matrix.charpoly("x")} \n'
 
-                data[(2 + 5*(9*x + 3*y + z))] = "a = " + str(a) + ", b = " + str(b) + ", c = " + str(c) + ", d = " + str(d) + ", e = " + str(e) + '\n'
+                                data[135] = str(int(data[135]) + 1)
 
-                data[(3 + 5*(9*x + 3*y + z))] = str(A.charpoly("x")) + '\n'
+                                # Overwrites the text file with our updated copy of the file that contains
+                                # a new realization.
+                                with open("output.txt", 'w') as file:
+                                    file.writelines(data)
 
-                # Overwrites the text file with our updated copy of the file that contains
-                # a new realization.
-                with open("output.txt", 'w') as file:
-                  file.writelines(data)
+                                # If we have obtained 27 realizations, we may stop ALL of the loops and thus
+                                # quit the program.
+                        if int(data[135]) == 27:
+                            return
 
-                # If we have obtained 27 realizations, we may stop ALL of the loops and thus
-                # quit the program.
-                if (trueVals == 27):
-                  return
 
-# This method takes our text file and wipes it clean. Then it will create the neccesary
-# number of newlines in the file so we can reference line numbers and check to see if 
+# This method takes our text file and wipes it clean. Then it will create the necessary
+# number of newlines in the file so we can reference line numbers and check to see if
 # we have found that specific realization yet (if the line equals '\n').
-def cleanFile():
-  with open("output.txt", 'w'):
-    pass
+def clean_file():
+    with open("output.txt", 'w'):
+        pass
 
-  with open("output.txt", 'a') as file:
-    for i in range(0, 134):
-      file.write('\n')
+    with open("output.txt", 'a') as file:
+        for i in range(135):
+            file.write('\n')
+            if i == 134:
+                file.write('0')
+
 
 main()
-
-
-                  
-
-
-
-
-
